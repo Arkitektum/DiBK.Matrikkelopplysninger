fa2c3330c3c2b74da72a927163d1f8bef5212cdf
Mari Aarak <mari.aarak@arkitektum.no>
2022-01-27 16:41:58 +0100

    Add API controller endpoint for matrikkel information response model

---
+++ DiBK.Matrikkelopplysninger/Controllers/MatrikkelController.cs
@@ -0,0 +1,17 @@
+﻿using DiBK.Matrikkelopplysninger.Services;
+using Microsoft.AspNetCore.Mvc;
+using no.kxml.skjema.dibk.matrikkelregistrering;
+
+namespace DiBK.Matrikkelopplysninger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatrikkelController : ControllerBase
+    {
+        [HttpGet("{knr:int},{gnr:int},{bnr:int}")]
+        public MatrikkelregistreringType Get(int knr, int gnr, int bnr)
+        {
+            return new MatrikkeldataProvider().GetMatrikkelOpplysninger(knr, gnr, bnr);
+        }
+    }
+}
