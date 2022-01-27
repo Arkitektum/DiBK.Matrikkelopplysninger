6e5e9ea6edafb63656857117049aa34d26b40cb2
Mari Aarak <mari.aarak@arkitektum.no>
2022-01-27 16:23:11 +0100

    Add matrikkel data provider

---
+++ DiBK.Matrikkelopplysninger/Services/MatrikkeldataProvider.cs
@@ -0,0 +1,22 @@
+﻿using no.kxml.skjema.dibk.matrikkelregistrering;
+
+namespace DiBK.Matrikkelopplysninger.Services;
+
+public class MatrikkeldataProvider
+{
+    public MatrikkelregistreringType GetMatrikkelOpplysninger(int knr, int gnr, int bnr)
+    {
+        return new MatrikkelregistreringType
+        {
+            eiendomsidentifikasjon = new MatrikkelnummerType[]
+            {
+                new()
+                {
+                    kommunenummer = knr.ToString(),
+                    gaardsnummer = gnr.ToString(),
+                    bruksnummer = bnr.ToString()
+                }
+            }
+        };
+    }
+}
---
+++ DiBk.MatrikkelOpplysningerTests/Services/MatrikkeldataProviderTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using DiBK.Matrikkelopplysninger.Services;
+using FluentAssertions;
+using Xunit;
+
+namespace DiBK.MatrikkelopplysningerTests.Services;
+
+public class MatrikkeldataProviderTests
+{
+    [Fact]
+    public void GetMatrikkelOpplysningerTest()
+    {
+        var matrikkelregistreringType = new MatrikkeldataProvider().GetMatrikkelOpplysninger(3817, 55, 13);
+
+        var matrikkelnummerType = matrikkelregistreringType.eiendomsidentifikasjon.First();
+
+        matrikkelnummerType.kommunenummer.Should().Be("3817");
+        matrikkelnummerType.gaardsnummer.Should().Be("55");
+        matrikkelnummerType.bruksnummer.Should().Be("13");
+    }
+}
